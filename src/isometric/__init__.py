674566b51a92035dbf1e8359177b26ec6ea9536b
Johannes <johannes-programming@posteo.org>
2023-12-19 16:49:55 +0100

    Version 1.2.0

--- src/isometric/__init__.py
+++ src/isometric/__init__.py
@@ -1,107 +1,93 @@
 import math as _math
 import typing as _typing
 
-
-class Description(_typing.NamedTuple):
-    x: _typing.Any = 0
-    y: _typing.Any = 0
-    z: _typing.Any = 0
-    def tare_x(self):
-        cls = type(self)
-        return cls(0, self.y - self.x, self.z - self.x)
-    def tare_y(self):
-        cls = type(self)
-        return cls(self.x - self.y, 0, self.z - self.y)
-    def tare_z(self):
-        cls = type(self)
-        return cls(self.x - self.z, self.y - self.z, 0)
-    def projected_abscissa(self):
-        return (self.y - self.x) * (.75 ** .5)
-    def projected_ordinate(self):
-        return (self.x * -.5) + (self.y * -.5) + self.z
-    def projected_radius(self):
-        return ((self.projected_abscissa() ** 2) + (self.projected_ordinate() ** 2)) ** .5
-    def projected_angle(self):
+class _Digest(_typing.NamedTuple):
+    x: float = 0.0
+    y: float = 0.0
+    def radius(self) -> float:
+        return ((a ** 2) for a in self) ** .5
+    def angle(self) -> float:
         try:
-            ratio = self.projected_abscissa() / self.projected_radius()
+            ratio = self.x / self.radius()
         except ZeroDivisionError:
             return float('nan')
-        if self.projected_ordinate() < 0:
-            return -_math.acos(ratio)
-        else:
-            return _math.acos(ratio)
-    def __bool__(self):
-        return all(self)
-    def __float__(self):
-        if bool(self):
-            return float('nan')
-        else:
-            return 0.
-    def __int__(self):
-        return int(float(self))
+        ans = _math.acos(ratio)
+        if self.y < 0:
+            ans *= -1
+        return ans
+
+class _Description(_typing.NamedTuple):
+    x: int = 0
+    y: int = 0
+    z: int = 0
     
 class Vector:
-    def tare_x(self):
-        return Description(0, self._y, self._z)
-    def tare_y(self):
-        return self.tare_x().tare_y()
-    def tare_z(self):
-        return self.tare_x().tare_z()
-    def projected_abscissa(self):
-        return self.tare_x().projected_abscissa()
-    def projected_ordinate(self):
-        return self.tare_x().projected_ordinate()
-    def projected_angle(self):
-        return self.tare_x().projected_angle()
-    def projected_radius(self):
-        return self.tare_x().projected_radius()
+    def digest(self):
+        x = (3 ** .5) * .5 * self._y
+        y = (-.5 * self._y) + self._z
+        ans = _Digest(x, y)
+        return ans
     def rotate(self, amount):
-        cls = type(self)
-        items = self._rotate(amount)[:3]
-        return cls(*items)
-    def _rotate(self, amount):
         cls = type(self)
         if type(amount) is not int:
             raise TypeError
-        v = self
+        ans = self
         if amount % 2:
-            v = -v
-            amount -= 3
-        items = tuple(v.tare_x()) * 2
+            amount += 3
+            ans = -ans
         amount %= 6
         if amount == 0:
-            return items[0:]
+            return cls(0, self._y, self._z)
         if amount == 2:
-            return items[1:]
+            return cls(self._z, 0, self._y)
         if amount == 4:
-            return items[2:]
+            return cls(self._y, self._z, 0)
         raise NotImplementedError
-    def _dot(self, other):
-        return (
-            (self._y * other._y)
-            + (self._z * other._z)
-            + (-.5 * self._y * other._z)
-            + (-.5 * self._z * other._y)
-        )
-    def _list(self):
-        return [
-            self.tare_x(),
-            self.tare_y(),
-            self.tare_z(),
-        ]
+    def tare(self, axis=0):
+        axis = {
+            0:0,
+            1:1,
+            2:2,
+            'x':0,
+            'y':1,
+            'z':2,
+        }[axis]
+        if axis == 0:
+            return _Description(0, self._y, self._z)
+        if axis == 1:
+            return _Description(-self._y, 0, self._z - self._y)
+        if axis == 2:
+            return _Description(-self._z, self._y - self._z, 0)
+        raise NotImplementedError
+    def hflip(self):
+        cls = type(self)
+        ans = cls(self._y, 0, self._z)
+        return ans
+    def vflip(self):
+        cls = type(self)
+        ans = cls(-self._y, 0, -self._z)
+        return ans
+    @classmethod
+    def linear_dependence(cls, *vectors):
+        errors = list()
+        for v in vectors:
+            if type(v) is not cls:
+                errors.append(TypeError(v))
+        if len(errors):
+            raise ExceptionGroup("Non-Vectors given!", errors)
+        if len(vectors) > 2:
+            return True
+        if len(vectors) < 2:
+            return not all(vectors)
+        v, w = vectors
+        return (v._y * w._z) == (v._z * w._y)
     def __init__(self, *args, **kwargs):
-        v = Description(*args, **kwargs)
-        self._y = v[1] - v[0]
-        self._z = v[2] - v[0]
-    def __iter__(self):
-        for a in self._list():
-            yield a
-    def __getitem__(self, key):
-        if type(key) is int:
-            return self._list()[key]
-        if type(key) is slice:
-            return (a for a in self._list())
-        raise TypeError
+        desc = _Description(*args, **kwargs)
+        for a in desc:
+            if type(a) is not int:
+                raise TypeError
+        self._y = desc.y - desc.x
+        self._z = desc.z - desc.x
     def __add__(self, other):
         cls = type(self)
         if type(other) is not cls:
@@ -114,19 +100,32 @@ class Vector:
         return self + (-other)
     def __mul__(self, other):
         cls = type(self)
-        if type(other) is cls:
-            return self._dot(other)
-        else:
-            return cls(0, self._y * other, self._z * other)
+        if type(other) is not cls:
+            return cls(
+                0, 
+                self._y * other, 
+                self._z * other,
+            )
+        return (
+            (self._y * other._y)
+            + (self._z * other._z)
+            + (-.5 * self._y * other._z)
+            + (-.5 * self._z * other._y)
+        )
     def __rmul__(self, other):
         cls = type(self)
-        if type(other) is cls:
-            raise NotImplementedError
-        else:
-            return cls(0, other * self._y, other * self._z)
+        return cls(
+            0, 
+            other * self._y, 
+            other * self._z,
+        )
     def __div__(self, other):
         cls = type(self)
-        return cls(0, self._y / other, self._z / other)
+        return cls(
+            0, 
+            self._y / other, 
+            self._z / other,
+        )
     def __pow__(self, other):
         if type(other) is not int:
             raise TypeError
@@ -136,25 +135,21 @@ class Vector:
         for i in range(other):
             ans *= self
         return ans
+    def __abs__(self):
+        pieces = {abs(a) for a in self.tare()}
+        if len(pieces) < 3:
+            return max(pieces)
+        return self.digest().radius()
     def __hash__(self):
-        return (self._y, self._z).__hash__()
+        return self.tare().__hash__()
     def __eq__(self, other):
         cls = type(self)
         if type(other) is not cls:
             return False
-        return (self._y == other._y) and (self._z == other._z)
-    def __str__(self):
-        cls = type(self)
-        return f"{cls.__name__}(projected_abscissa={self.projected_abscissa()}, projected_ordinate={self.projected_ordinate()})"
-    def __repr__(self):
-        return str(self)
+        return self.tare() == self.tare()
     def __bool__(self):
-        return self._y or self._z
+        return any(self.tare())
     def __float__(self):
-        if bool(self):
-            return float('nan')
-        else:
-            return 0.
+        return float('nan') if self else .0
     def __int__(self):
         return int(float(self))
-        
